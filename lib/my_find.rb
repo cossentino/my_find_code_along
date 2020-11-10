fecf5b07a8432adc5db7717b9f4059d25e9583c4
Ian Cossentino <icossentino@gmail.com>
2020-11-10 15:15:22 -0500

    commit tree 88b0a1d56c441a2f08cdf014c90237d33bcb6410

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+    found_array = []
+    collection.each do |e|
+        if yield(e)
+            return e
+        else
+            next
+        end
+    end
+    return nil
 end
